5ffa6a3bd4c377de8b7264cbd6c1efd600036bbc
RoyvandenHeuvel <0878963@hr.nl>
2017-09-12 16:14:59 +0200

    .

--- Assets/Scripts/UI Scripts/ScoreDisplayScript.cs
+++ Assets/Scripts/UI Scripts/ScoreDisplayScript.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Scripts;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,17 +7,15 @@ using UnityEngine.UI;
 public class ScoreDisplayScript : MonoBehaviour
 {
     private Text _text;
-    private GameObject _player;
 
     void Start()
     {
-        _player = GameObject.Find("Player");
         _text = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _text.text = string.Format("{0}", Math.Round(_player.transform.position.y));
+        _text.text = string.Format("{0}", GameManager.Instance.GameData.Coins);
     }
 }
