f3a27fc2c80f45299e35f6c3ff11e7ca8ea1a8c1
RoyvandenHeuvel <0878963@hr.nl>
2017-09-13 10:39:17 +0200

    4-way Dpad controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,30 +1,29 @@
 ﻿using UnityEngine;
 using Assets.Scripts;
+using CnControls;
 
 public class Player : MonoBehaviour
 {
-    public VJHandler JsMovement;
-
-    private Vector3 direction;
     private float xMin, xMax, yMin, yMax;
 
     void Update()
     {
+        Vector3 newPosition = gameObject.transform.position;
+
+        // Setting the boundaries.
         xMax = 2.8f;
         yMax = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight)).y;
 
         xMin = -2.8f;
-        if(yMin == default(float) || Camera.main.ScreenToWorldPoint(Vector3.zero).y > yMin)
+        if (yMin == default(float) || Camera.main.ScreenToWorldPoint(Vector3.zero).y > yMin)
         {
             yMin = Camera.main.ScreenToWorldPoint(Vector3.zero).y;
         }
 
-        direction = JsMovement.InputDirection; 
-        
-        if (direction.magnitude != 0)
-        {
-            transform.position += direction * GameManager.Instance.GameData.PlayerSpeed;
-        }
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), Mathf.Clamp(transform.position.y + GameManager.Instance.GameData.CameraSpeed, yMin, yMax), 0f);
+        newPosition.x += GameManager.Instance.GameData.PlayerSpeed * CnInputManager.GetAxis("Horizontal");
+        newPosition.y += (GameManager.Instance.GameData.PlayerSpeed * CnInputManager.GetAxis("Vertical")) + GameManager.Instance.GameData.CameraSpeed;
+        newPosition = new Vector3(Mathf.Clamp(newPosition.x, xMin, xMax), Mathf.Clamp(newPosition.y, yMin, yMax), 0f);
+
+        transform.position = newPosition;
     }
 }
