98fc37c22356f98d896f9d6700c17301b309c00c
RoyvandenHeuvel <0878963@hr.nl>
2017-09-13 13:22:42 +0200

    Basic ability implemented.

--- Assets/Scripts/BasicAbilities.cs
+++ Assets/Scripts/BasicAbilities.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using CnControls;
 using Assets.Scripts;
+using UnityEngine.UI;
 
 public class BasicAbilities : MonoBehaviour
 {
@@ -18,11 +19,13 @@ public class BasicAbilities : MonoBehaviour
     public Ability ChosenAbility;
     public float Cooldown;
 
+    private Image _buttonImage;
     private float _timeSinceLastUse;
 
     private void Start()
     {
         // Starting off with player being able to use their ability.
+        _buttonImage = GameObject.Find("BasicAbility").GetComponent<Image>();
         _timeSinceLastUse = Cooldown;
     }
 
@@ -30,13 +33,19 @@ public class BasicAbilities : MonoBehaviour
     {
         if (_timeSinceLastUse < Cooldown * 1.5f)
             _timeSinceLastUse += Time.deltaTime;
+        if (_timeSinceLastUse < Cooldown)
+        {
+            _buttonImage.CrossFadeAlpha(0.1f, Cooldown, false);
+        }
+
         if (_timeSinceLastUse >= Cooldown)
         {
-            Ability wantedAction = GetAbility();
-            if (wantedAction == Ability.Nothing)
+            _buttonImage.CrossFadeAlpha(1f, Cooldown, false);
+            Ability toUseAbility = GetAbility();
+            if (toUseAbility == Ability.Nothing)
                 return;
-            if ((wantedAction != ChosenAbility) || (_timeSinceLastUse >= 1.0f))
-                PerformAbility(wantedAction);
+            if (toUseAbility == ChosenAbility)
+                PerformAbility(toUseAbility);
         }
     }
 
@@ -44,18 +53,17 @@ public class BasicAbilities : MonoBehaviour
     {
         if (CnInputManager.GetButtonDown(ButtonName))
         {
-            return Ability.BasicAttack;
+            return ChosenAbility;
         }
 
         return Ability.Nothing;
     }
 
-    private void PerformAbility(Ability action)
+    private void PerformAbility(Ability ability)
     {
-        ChosenAbility = action;
         _timeSinceLastUse = 0;
 
-        switch (ChosenAbility)
+        switch (ability)
         {
             case Ability.BasicAttack:
                 BasicAttack();
@@ -68,12 +76,10 @@ public class BasicAbilities : MonoBehaviour
     private void BasicAttack()
     {
         var enemy = GameObject.Find("Enemy").GetComponent<Enemy>();
-
         // TODO: Play animation probably.
-
         if (enemy.IsInRange(BasicAttack_Range, this.gameObject.transform))
         {
-            Vector3 direction = transform.position - this.gameObject.transform.position;
+            Vector3 direction = enemy.transform.position - this.gameObject.transform.position;
             direction.Normalize();
 
             enemy.gameObject.transform.position += direction * BasicAttack_KnockbackStrength;
