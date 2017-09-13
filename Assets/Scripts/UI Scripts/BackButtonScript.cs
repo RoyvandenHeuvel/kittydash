ab60e04d56e5db14dcd445c0c6eb76472f23e9ba
RoyvandenHeuvel <0878963@hr.nl>
2017-09-13 16:13:09 +0200

    Back button functionality added.

---
+++ Assets/Scripts/UI Scripts/BackButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButtonScript : MonoBehaviour
+{
+    public string sceneName;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            KittyDashSceneManager.LoadSceneOrQuit(sceneName);       
+        }
+    }
+}
+
+public class KittyDashSceneManager : SceneManager
+{
+    public static void LoadSceneOrQuit(string sceneName)
+    {
+        if (!SceneManager.GetActiveScene().name.Equals(sceneName))
+        {
+            KittyDashSceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
