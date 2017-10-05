544d8e68fe26b02cf0886b439bdab34dd07e5f54
RoyvandenHeuvel <0878963@hr.nl>
2017-10-05 14:57:54 +0200

    Added some achievements

---
+++ Assets/AchievementButton.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts;
+using GooglePlayGames;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class AchievementButton : MonoBehaviour
+{
+
+    private Button _button;
+
+    private void Start()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(GameManager.Instance.ShowAchievements);
+    }
+
+    private void Update()
+    {
+        _button.interactable = PlayGamesPlatform.Instance.IsAuthenticated();
+    }
+
+}
